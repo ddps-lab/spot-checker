321ebc2b5ab497cd42892d5785a6d411c19f1182
red0sena <bryan9801@kookmin.ac.kr>
2023-09-24 20:35:56 +0900

    update spot-dingdongditch.py add ding-dong period

--- aws/spot-dingdongditch/run_workloads_parallel.sh
+++ aws/spot-dingdongditch/run_workloads_parallel.sh
@@ -1,4 +1,4 @@
-while IFS=" " read instance region az_id
+while IFS=" " read instance region az_id ding_dong_period
 do
-    python3 spot-dingdongditch.py --instance_type=$instance --region=$region --az_id=$az_id &
-done < ../data/workloads_10.txt
+    python3 spot-dingdongditch.py --instance_type=$instance --region=$region --az_id=$az_id --ding_dong_period=$ding_dong_period &
+done < ./workloads_10.txt
--- aws/spot-dingdongditch/spot-dingdongditch.py
+++ aws/spot-dingdongditch/spot-dingdongditch.py
@@ -4,8 +4,9 @@ import pickle
 import datetime
 import argparse
 import pandas as pd
-from pathlib import Path
 import os
+import pytz
+from pathlib import Path
 from slack_msg_sender import send_slack_message
 
 
@@ -31,9 +32,11 @@ def save_status_code(spot_data_list, instance_type, az_id):
 
     send_slack_message(instance_type, az_id, launch_time.strftime('%Y-%m-%D %H:%M:%S'), "로컬에 로그 저장 완료.")
 
+
+
+
 # create spot instance
 def create_spot_instance(instance_type, ami_id, az_name, az_id):
-
     launch_spec = {
         'ImageId': ami_id,
         'InstanceType': instance_type,
@@ -45,11 +48,11 @@ def create_spot_instance(instance_type, ami_id, az_name, az_id):
         LaunchSpecification=launch_spec,
         Type='one-time'
     )
+
     request_id = create_request_response['SpotInstanceRequests'][0]['SpotInstanceRequestId']
 
     spot_data_list = []
     wait_time = 0
-
     while True:
         time.sleep(5)
         wait_time += 5
@@ -58,7 +61,7 @@ def create_spot_instance(instance_type, ami_id, az_name, az_id):
         spot_data_list.append([instance_type, az_id, code, datetime.datetime.utcnow()])
 
         if code == 'fulfilled' or code == 'capacity-not-available':
-            send_slack_message(instance_type, az_id, launch_time.strftime('%Y-%m-%D %H:%M:%S'), "fulfill, interrupt 됨")
+            send_slack_message(instance_type, az_id, launch_time.strftime('%Y-%m-%D %H:%M:%S'), f"{code} 됨")
             save_status_code(spot_data_list, instance_type, az_id)
             break
         if wait_time > 60:
@@ -78,6 +81,26 @@ def create_spot_instance(instance_type, ami_id, az_name, az_id):
     except Exception as e:
         send_slack_message(instance_type, az_id, launch_time.strftime('%Y-%m-%D %H:%M:%S'), f"!!!스팟 요청 삭제 실패!!! \n{e}")
 
+def spot_ding_dong_main(instance_type, ami_id, az_name, az_id, ding_dong_period):
+
+
+    create_spot_instance(instance_type, ami_id, az_name, az_id)
+    stop_time = datetime.datetime.now() + datetime.timedelta(hours=24)
+    stop_time = stop_time.astimezone(pytz.UTC)
+    next_ding_dong_time = launch_time.astimezone(pytz.UTC)
+    while True:
+        current_time = datetime.datetime.now()
+        current_time = current_time.astimezone(pytz.UTC)
+
+        if current_time > next_ding_dong_time:
+            create_spot_instance(instance_type, ami_id, az_name, az_id)
+            next_ding_dong_time = current_time + datetime.timedelta(minutes=ding_dong_period)
+
+        if current_time > stop_time:
+            send_slack_message(instance_type, az_id, launch_time.strftime('%Y-%m-%D %H:%M:%S'), "실험 인스턴스 정지")
+            break
+
+
 
 if __name__ == "__main__":
     region_ami = pickle.load(open('../data/region_ami_dict.pkl', 'rb'))  # {x86/arm: {region: (ami-id, ami-info), ...}}
@@ -89,6 +112,7 @@ if __name__ == "__main__":
     parser.add_argument('--instance_type', type=str, default='t2.large')
     parser.add_argument('--region', type=str, default='ap-southeast-2')
     parser.add_argument('--az_id', type=str, default='apse2-az2')
+    parser.add_argument('--ding_dong_period', type=int, default=30)
     args = parser.parse_args()
 
     ### Spot Checker Arguments Parsing
@@ -97,8 +121,10 @@ if __name__ == "__main__":
     instance_arch = 'arm' if (instance_family in arm64_family) else 'x86'
     region = args.region
     az_id = args.az_id
+    ding_dong_period = args.ding_dong_period
+
     az_name = az_map_dict[(region, az_id)]
     ami_id = region_ami[instance_arch][region][0]
 
     send_slack_message(instance_type, az_id, launch_time.strftime('%Y-%m-%D %H:%M:%S'), "시작")
-    create_spot_instance(instance_type, ami_id, az_name, az_id)
+    spot_ding_dong_main(instance_type, ami_id, az_name, az_id, ding_dong_period)
