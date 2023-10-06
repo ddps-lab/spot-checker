60460b211b38186da67bbbc8df74b1254e75a21a
mhsong <mhsong@kookmin.ac.kr>
2023-10-06 17:45:25 +0900

    Add deletion check & delete some undelete log group

--- spot-availability-tester/aws/delete_tester.py
+++ spot-availability-tester/aws/delete_tester.py
@@ -3,6 +3,7 @@ import sys
 import os
 import pandas as pd
 import variables
+import boto3
 
 def run_command(command):
     process = subprocess.Popen(
@@ -16,6 +17,16 @@ def run_command(command):
     else:
         print(out.decode())
 
+def delete_cloudwatch_log_group(log_group_name, logs_client):
+    try:
+        response = logs_client.delete_log_group(
+            logGroupName=f"/aws/lambda/{log_group_name}"
+        )
+        print(f"Log group {log_group_name} deleted successfully.")
+    except logs_client.exceptions.ResourceNotFoundException:
+        print(f"Log group {log_group_name} does not exist.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 def main():
     awscli_profile = variables.awscli_profile
@@ -37,6 +48,10 @@ def main():
     os.chdir(tf_project_dir)
 
     for region in regions:
+        boto3_session = boto3.Session(profile_name=awscli_profile, region_name=region)
+        logs_client = boto3_session('logs')
+        delete_cloudwatch_log_group(f"{prefix}-spot-availability-tester", logs_client)
+        delete_cloudwatch_log_group(f"{prefix}-terminate-no-name-instances", logs_client)
         run_command(["terraform", "workspace", "select", f"{region}"])
         run_command(["terraform", "destroy", "--auto-approve", "--var", f"region={region}", "--var", f"prefix={prefix}","--var", f"awscli_profile={awscli_profile}", "--var", f"log_group_name={log_group_name}", "--var", f"log_stream_name={log_stream_name}", "--var", f"instance_types=[{instance_type_data[region]}]", "--var", f"instance_types_az=[{availability_zone_data[region]}]"])
         run_command(["terraform", "workspace", "select", "default"])
@@ -44,4 +59,8 @@ def main():
 
 
 if __name__ == "__main__":
+    delete_check = input("Do you want to remove all resources??? (y/n) : ")
+    if delete_check != "y":
+        print("Interrupted!")
+        os._exit(0)
     main()
