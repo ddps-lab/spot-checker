6efe4f3ae533ab599002e85af3abbeae156faec4
mhsong <mhsong@kookmin.ac.kr>
2023-10-06 21:57:17 +0900

    Add generate test data set from SPS dataset

--- spot-availability-tester/aws/generate_test_data.py
+++ spot-availability-tester/aws/generate_test_data.py
@@ -1,13 +1,8 @@
 import pandas as pd
 import pickle
-
-region = "us-east-1"
+import os
 
 az_map_dict = pickle.load(open('./dataset/az_map_dict.pkl', 'rb'))
-
-low_data = pd.read_csv(f'./dataset/{region}/sps_low.csv')
-medium_data = pd.read_csv(f'./dataset/{region}/sps_medium.csv')
-high_data = pd.read_csv(f'./dataset/{region}/sps_high.csv')
 
 def az_id_to_actual_name(az_id, region):
     az_name = az_map_dict[(region, az_id)]
@@ -17,14 +12,30 @@ def extract_and_transform_actual_name(df):
     df['AZ'] = df.apply(lambda row: az_id_to_actual_name(row['AZ'], row['Region']), axis=1)
     return df[['InstanceType','AZ']]
 
-# Extract and transform data from each file using the updated function
-transformed_low_actual = extract_and_transform_actual_name(low_data)
-transformed_medium_actual = extract_and_transform_actual_name(medium_data)
-transformed_high_actual = extract_and_transform_actual_name(high_data)
+with open('regions.txt', 'r', encoding='utf-8') as file:
+    regions = [line.strip() for line in file.readlines()]
 
-# Concatenate the transformed dataframes
-combined_transformed_actual_data = pd.concat([transformed_high_actual.head(10), transformed_low_actual.head(10), transformed_medium_actual.head(10)], ignore_index=True)
-# combined_transformed_actual_data = pd.concat([transformed_high_actual, transformed_low_actual, transformed_medium_actual], ignore_index=True)
+instance_type_data = {}
+availability_zone_data = {}
 
-combined_transformed_actual_output_path = f"./test_data/{region}.csv"
-combined_transformed_actual_data.to_csv(combined_transformed_actual_output_path, index=False)
+if not os.path.exists(f'./result_data/{result_folder_path}'):
+    os.makedirs(f'./result_data/{result_folder_path}')
+
+for region in regions:
+    # low_data = pd.read_csv(f'./dataset/{region}/sps_low.csv')
+    # medium_data = pd.read_csv(f'./dataset/{region}/sps_medium.csv')
+    high_data = pd.read_csv(f'./dataset/{region}/sps_high.csv')
+
+
+    # Extract and transform data from each file using the updated function
+    # transformed_low_actual = extract_and_transform_actual_name(low_data)
+    # transformed_medium_actual = extract_and_transform_actual_name(medium_data)
+    transformed_high_actual = extract_and_transform_actual_name(high_data)
+
+    # Concatenate the transformed dataframes
+    # combined_transformed_actual_data = pd.concat([transformed_high_actual.head(10), transformed_low_actual.head(10), transformed_medium_actual.head(10)], ignore_index=True)
+    combined_transformed_actual_data = pd.concat([transformed_high_actual], ignore_index=True)
+    # combined_transformed_actual_data = pd.concat([transformed_high_actual, transformed_low_actual, transformed_medium_actual], ignore_index=True)
+
+    combined_transformed_actual_output_path = f"./test_data/{region}.csv"
+    combined_transformed_actual_data.to_csv(combined_transformed_actual_output_path, index=False)
