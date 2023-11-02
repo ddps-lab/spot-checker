23b8a0872ce6efdb167197d1b929ccd90bbf7b36
mhsong <mhsong@kookmin.ac.kr>
2023-11-02 19:43:29 +0900

    Add spot quota checker

---
+++ spot-quota-checker/aws/get_all_vcpu_info_by_instance_types.py
@@ -0,0 +1,59 @@
+import boto3
+import pickle
+import variables
+
+AWS_CLI_PROFILE_NAME = variables.awscli_profile
+
+def get_all_instance_types(boto3_session):
+    ec2_client = boto3_session.client('ec2')
+    instance_types_data = {}
+    next_token = None
+
+    while True:
+        if next_token:
+            response = ec2_client.describe_instance_types(NextToken=next_token, MaxResults=100)
+        else:
+            response = ec2_client.describe_instance_types(MaxResults=100)
+        
+        for instance_type in response['InstanceTypes']:
+            instance_type_name = instance_type['InstanceType']
+            vcpu_count = instance_type['VCpuInfo']['DefaultVCpus']
+            instance_types_data[instance_type_name] = vcpu_count
+        
+        next_token = response.get('NextToken')  # 다음 페이지를 가져오기 위한 토큰
+        if not next_token:
+            break  # 더 이상의 페이지가 없으면 루프를 종료한다.
+
+    return instance_types_data
+
+
+boto3_session_list = []
+with open('regions.txt.sample', 'r', encoding='utf-8') as file:
+    regions = [line.strip() for line in file.readlines()]
+
+for region in regions:
+    if AWS_CLI_PROFILE_NAME == None:
+        boto3_session = boto3.Session(region_name=region)
+    else:
+        boto3_session = boto3.Session(
+            profile_name=AWS_CLI_PROFILE_NAME, region_name=region)
+    boto3_session_list.append(boto3_session)
+
+all_instance_types_data = {}
+for region_index, region in enumerate(regions):
+    instance_types_data = get_all_instance_types(boto3_session_list[region_index])
+    all_instance_types_data[region] = instance_types_data
+
+# def get_vcpu_count(region, instance_type):
+#     return all_instance_types_data.get(region, {}).get(instance_type, None)
+
+# region = 'ap-northeast-2'
+# instance_type = 't2.micro'
+# vcpu_count = get_vcpu_count(region, instance_type)
+# if vcpu_count is not None:
+#     print(f'The {instance_type} instance type has {vcpu_count} vCPUs.')
+# else:
+#     print(f'Unknown instance type: {instance_type}')
+
+with open('all_vcpu_info.pkl', 'wb') as f:
+    pickle.dump(all_instance_types_data, f)
---
+++ spot-quota-checker/aws/spot_quota_checker.py
@@ -0,0 +1,163 @@
+# Not For Lambda, Just Run in AWS EC2
+# Need regions.txt in same directory
+import re
+import boto3
+import spot_quota_checker_variables
+import time
+import pickle
+import json
+
+AWS_CLI_PROFILE_NAME = spot_quota_checker_variables.awscli_profile
+LOG_GROUP_NAME = f"{spot_quota_checker_variables.prefix}-spot-quota-checker-log"
+LOG_STREAM_NAME = spot_quota_checker_variables.log_stream_name
+with open('all_vcpu_info.pkl', 'rb') as f:
+    ALL_VCPU_INFO = pickle.load(f)
+with open('all_vcpu_quota_info.pkl', 'rb') as f:
+    ALL_QUOTA_INFO = pickle.load(f)
+
+def get_vcpu_number(region,instance_type):
+    return int(ALL_VCPU_INFO.get(region, {}).get(instance_type, None))
+
+def get_quota_number(region,quota_name):
+    return int(ALL_QUOTA_INFO.get(region, {}).get(quota_name, None))
+
+def create_log_event(result, boto3_session):
+    logs_client = boto3_session.client('logs')
+    log_event = {
+        'timestamp': int(time.time() * 1000),
+        'message': result
+    }
+    logs_client.put_log_events(
+        logGroupName=LOG_GROUP_NAME, logStreamName=LOG_STREAM_NAME, logEvents=[log_event])
+
+def check_spot_quota(region, boto3_session):
+    ec2_client = boto3_session.client('ec2')
+    
+    vCPU_Counts = {
+        "INF": 0,
+        "TRN": 0,
+        "DL": 0,
+        "G_VT": 0,
+        "P5": 0,
+        "P2_P3_P4": 0,
+        "F": 0,
+        "X": 0,
+        "STANDARD": 0
+    }
+
+    while True:
+        try:
+            spot_request_response = ec2_client.describe_spot_instance_requests(
+                Filters=[{
+                    'Name': 'state',
+                    'Values': ['open', 'active', 'failed']
+                }])
+            break
+        except:
+            time.sleep(1)
+    while True:
+        try:
+            ec2_instance_response = ec2_client.describe_instances(
+                Filters=[
+                    {
+                        'Name': 'instance-state-name',
+                        'Values': ['pending', 'running', 'shutting-down', 'stopping', 'stopped']
+                    },
+                    {
+                        'Name': 'instance-lifecycle',
+                        'Values': ['spot']
+                    }
+                ])
+            break
+        except:
+            time.sleep(1)
+    
+    for spot_request in spot_request_response['SpotInstanceRequests']:
+        instance_type = spot_request['LaunchSpecification']['InstanceType']
+        vcpu_num = get_vcpu_number(region, instance_type)
+        
+        if instance_type[:3] == 'trn':
+            vCPU_Counts['TRN'] = vCPU_Counts['TRN'] + vcpu_num
+        elif instance_type[:3] == 'inf':
+            vCPU_Counts['INF'] = vCPU_Counts['INF'] + vcpu_num
+        elif instance_type[:2] == 'dl':
+            vCPU_Counts['DL'] = vCPU_Counts['DL'] + vcpu_num
+        elif instance_type[:2] == 'vt':
+            vCPU_Counts['G_VT'] = vCPU_Counts['G_VT'] + vcpu_num
+        elif instance_type[:1] == 'g':
+            vCPU_Counts['G_VT'] = vCPU_Counts['G_VT'] + vcpu_num
+        elif instance_type[:1] == 'p':
+            if instance_type[1] == '5':
+                vCPU_Counts['P5'] = vCPU_Counts['P5'] + vcpu_num
+            else:
+                vCPU_Counts['P2_P3_P4'] = vCPU_Counts['P2_P3_P4'] + vcpu_num
+        elif instance_type[:1] == 'f':
+            vCPU_Counts['F'] = vCPU_Counts['F'] + vcpu_num
+        elif instance_type[:1] == 'x':
+            vCPU_Counts['X'] = vCPU_Counts['X'] + vcpu_num
+        else:
+            vCPU_Counts['STANDARD'] = vCPU_Counts['STANDARD'] + vcpu_num
+        
+    
+    for ec2_instance in ec2_instance_response['Reservations']:
+        spot_duplicate_check = 0
+        for spot_request in spot_request_response['SpotInstanceRequests']:
+            if spot_request['InstanceId'] == ec2_instance['Instances'][0]['InstanceId']:
+                spot_duplicate_check = 1
+        if spot_duplicate_check == 1:
+            continue
+        instance_type = ec2_instance['Instances'][0]['InstanceType']
+        vcpu_num = get_vcpu_number(region, instance_type)
+
+        if instance_type[:3] == 'trn':
+            vCPU_Counts['TRN'] = vCPU_Counts['TRN'] + vcpu_num
+        elif instance_type[:3] == 'inf':
+            vCPU_Counts['INF'] = vCPU_Counts['INF'] + vcpu_num
+        elif instance_type[:2] == 'dl':
+            vCPU_Counts['DL'] = vCPU_Counts['DL'] + vcpu_num
+        elif instance_type[:2] == 'vt':
+            vCPU_Counts['G_VT'] = vCPU_Counts['G_VT'] + vcpu_num
+        elif instance_type[:1] == 'g':
+            vCPU_Counts['G_VT'] = vCPU_Counts['G_VT'] + vcpu_num
+        elif instance_type[:1] == 'p':
+            if instance_type[1] == '5':
+                vCPU_Counts['P5'] = vCPU_Counts['P5'] + vcpu_num
+            else:
+                vCPU_Counts['P2_P3_P4'] = vCPU_Counts['P2_P3_P4'] + vcpu_num
+        elif instance_type[:1] == 'f':
+            vCPU_Counts['F'] = vCPU_Counts['F'] + vcpu_num
+        elif instance_type[:1] == 'x':
+            vCPU_Counts['X'] = vCPU_Counts['X'] + vcpu_num
+        else:
+            vCPU_Counts['STANDARD'] = vCPU_Counts['STANDARD'] + vcpu_num
+
+    result = {
+        "Region": region,
+        "vCPU_Count": vCPU_Counts,
+        "Timestamp": time.time(),
+    }
+    return result
+
+
+def main():
+    boto3_session_list = []
+    with open('regions.txt', 'r', encoding='utf-8') as file:
+        regions = [line.strip() for line in file.readlines()]
+
+    for region in regions:
+        if AWS_CLI_PROFILE_NAME == None:
+            boto3_session = boto3.Session(region_name=region)
+        else:
+            boto3_session = boto3.Session(
+                profile_name=AWS_CLI_PROFILE_NAME, region_name=region)
+        boto3_session_list.append(boto3_session)
+    while True:
+        for region_index, region in enumerate(regions):
+            result = check_spot_quota(region, boto3_session_list[region_index])
+            create_log_event(json.dumps(result), boto3_session_list[region_index])
+            print(result)
+        time.sleep(5)
+
+
+if __name__ == '__main__':
+    main()
