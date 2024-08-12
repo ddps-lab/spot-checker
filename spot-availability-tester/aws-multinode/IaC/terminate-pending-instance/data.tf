8d943322f21179f5e50873b1c200aec5649d45a6
mhsong <mhsong@kookmin.ac.kr>
2024-08-12 23:54:21 +0900

    Remove new multi node ddd

--- spot-availability-tester/aws-multinode/IaC/terminate-pending-instance/data.tf
+++
@@ -1,5 +0,0 @@
-data "archive_file" "lambda_source_code" {
-  type        = "zip"
-  source_file = "terminate-pending-instances.py"
-  output_path = "terminate-pending-instances.zip"
-}
--- spot-availability-tester/aws-multinode/IaC/tester-ec2/var.tf
+++
@@ -1,5 +0,0 @@
-variable "prefix" {}
-variable "vpc_id" {}
-variable "subnet_ids" {}
-variable "region" {}
-variable "iam_role" {}
